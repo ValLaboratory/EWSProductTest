7cd6e4bc048daed72d7482e0a25706b902684cde
val00280 <tanabe@val.co.jp>
2014-07-01 13:48:19 +0900

    融合版と旧版で同一の内容でもレスポンスサイズに差が出る現象に対応しました。
    schemaがXMLの場合はXML宣言のstandalone属性の有無、
    JSONの場合は:や,などの記号後のスペースの有無でレスポンスサイズに差が出ます。
    XMLならレスポンスサイズが小さい(200バイト以下)場合、
    JSONは要素数が多い(=区切るための記号が多い)場合に、
    内容が同一だとしても10％以上の誤差が出る可能性があります。
    
    融合版のXMLのレスポンスのstandalone属性はあった方が良いので残しますが、
    旧版との比較時にサイズでエラーになってしまう可能性があります。
    切替前のチェックとしては、
    融合版のレスポンスからstandalone属性を
    削除してからボディサイズを比較します。
    
    融合版のJSONのレスポンスに存在する記号後のスペースは
    無駄に容量が増えるだけなので無くす予定ですが、
    その修正を切替前に行こなうかどうか現時点で未定なので、
    とりあえず削除してボディサイズを比較するようにしておきます。
    
    また、「URLをGETした結果を確認」のshared_examplesの
    利用例として書かれていたテストを削除して、
    ログからのテストだけを行うようにしました。

--- spec/log_test.rb
+++ spec/log_test.rb
@@ -4,38 +4,8 @@ require 'spec_helper'
 require 'json'
 require 'erb'
 
-list = [
-	{
-		'method_query'=> '/v1/xml/station/light?key=AMP5FznGPXeT6psm&name=\x9e\xd6\x83\x96\x89\xaa&type=train',
-		'check_targets'=> {
-			'http_status'=>'200',
-			'body_size'=>{ 
-				'size'=>117,
-				'range'=>10
-			}
-		}
-	},
-	{
-		'method_query'=> '/v1/xml/corporation?key=wC4SR9ETBhBcJ3Bv&code=1',
-		'check_targets'=> {
-			'http_status'=>'200',
-			'body_size'=>{
-				'size'=>200,
-				'range'=>10
-			}
-		}
-	}
-]
-
-
 
 describe "ログからWebサービスURLテスト" do
-	test_list = list
-	test_list.each do |test_record|
-		describe "#{test_record['method_query']}" do
-			it_behaves_like 'URLをGETした結果を確認', test_record
-		end
-	end
   File.open(log_file_path) do |file|
     tested_method_query = {}
     file.each_line do |line|
